32db122a709c4794fa3ae704425c6d456289cd22
Destrifer <destrifer@yandex.ru>
2025-05-01 19:11:29 +0300

    add hover scale

--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -5,12 +5,12 @@
     >
       <!-- Лого или текст -->
       <div class="flex items-center">
-        <NuxtLink to="/" class="lg:hidden">
+        <NuxtLink to="/" class="lg:hidden hover-scale">
           <img src="/logo.svg" class="w-10 h-10" alt="Logo" />
         </NuxtLink>
         <NuxtLink
           to="/"
-          class="hidden lg:block font-bold text-md-fluid font-display"
+          class="hidden lg:block font-bold text-md-fluid font-display hover-scale"
         >
           CentralPrint
         </NuxtLink>
@@ -25,7 +25,7 @@
       <nav aria-label="Контакты" class="flex gap-4 items-center text-sm-fluid">
         <a
           href="https://t.me/yourbot"
-          class="flex items-center"
+          class="flex items-center hover-scale"
           target="_blank"
           aria-label="Telegram"
         >
@@ -33,7 +33,7 @@
         </a>
         <a
           href="https://wa.me/79998887766"
-          class="flex items-center"
+          class="flex items-center hover-scale"
           target="_blank"
           aria-label="WhatsApp"
         >
@@ -41,7 +41,10 @@
         </a>
 
         <!-- Телефон -->
-        <a href="tel:+79998887766" class="flex items-center gap-2 group">
+        <a
+          href="tel:+79998887766"
+          class="flex items-center gap-2 group hover-scale"
+        >
           <Icon name="f7:phone-circle-fill" size="36" />
           <span class="hidden xl:inline font-semibold">+7 (999) 888-77-66</span>
         </a>
@@ -49,7 +52,7 @@
         <!-- Почта -->
         <a
           href="mailto:info@centralprint.ru"
-          class="flex items-center gap-2 group"
+          class="flex items-center gap-2 group hover-scale"
         >
           <Icon name="f7:envelope-circle-fill" size="36" />
           <span class="hidden xl:inline font-semibold"
@@ -61,7 +64,7 @@
       <!-- Кнопка с текстом (ПК) -->
       <NuxtLink
         to="/checkout"
-        class="hidden sm:flex gap-2 items-center uppercase ml-4 bg-text text-primary py-2 px-4 rounded-full font-semibold hover:bg-danger transition"
+        class="hidden sm:flex gap-2 items-center uppercase ml-4 bg-text text-primary py-2 px-4 rounded-full font-semibold hover:bg-danger transition hover-scale"
       >
         <Icon name="solar:cart-5-outline" size="24" />
         Заказать
@@ -70,14 +73,16 @@
       <!-- Круглая иконка (мобайл) -->
       <NuxtLink
         to="/checkout"
-        class="flex sm:hidden items-center justify-center bg-text text-primary w-9 h-9 rounded-full hover:bg-danger transition"
+        class="flex sm:hidden items-center justify-center bg-text text-primary w-9 h-9 rounded-full hover:bg-danger transition hover-scale"
         aria-label="Заказать"
       >
         <Icon name="solar:cart-5-outline" size="20" />
       </NuxtLink>
 
       <!-- Бургер -->
-      <button @click="toggleMenu" class="md:hidden text-3xl ml-4">☰</button>
+      <button @click="toggleMenu" class="md:hidden text-3xl ml-4 hover-scale">
+        ☰
+      </button>
     </div>
 
     <!-- Бургер-меню -->
