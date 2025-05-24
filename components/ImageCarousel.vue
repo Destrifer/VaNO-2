e79ed1a3d393442e0d99db4fa26a22ced3dc0977
Destrifer <destrifer@yandex.ru>
2025-05-24 19:53:39 +0300

    add lightbox

--- components/ImageCarousel.vue
+++ components/ImageCarousel.vue
@@ -1,7 +1,8 @@
-// components/ImageCarousel.vue
 <script setup>
 import { ref, onMounted, nextTick } from "vue";
 import emblaCarousel from "embla-carousel";
+import { Fancybox } from "@fancyapps/ui";
+import "@fancyapps/ui/dist/fancybox/fancybox.css";
 import { NuxtImg } from "#components";
 
 const props = defineProps({
@@ -16,31 +17,47 @@ const embla = ref(null);
 
 onMounted(async () => {
   await nextTick();
+
   if (emblaRef.value && !embla.value) {
     embla.value = emblaCarousel(emblaRef.value, {
       loop: true,
     });
   }
+
+  Fancybox.bind("[data-fancybox=gallery]", {
+    Thumbs: false,
+    Toolbar: false,
+    animated: false,
+    dragToClose: true,
+    click: false,
+    idle: false,
+    zoom: false,
+    Carousel: {
+      transition: "fade",
+    },
+  });
 });
 </script>
 
 <template>
   <div ref="emblaRef" class="overflow-hidden">
     <div class="flex">
-      <div
-        v-for="img in images"
-        :key="img"
+      <a
+        v-for="(img, index) in images"
+        :key="index"
+        :href="img"
+        data-fancybox="gallery"
         class="basis-1/2 sm:basis-1/3 lg:basis-1/4 shrink-0 px-2"
       >
         <NuxtImg
           :src="img"
           format="webp"
-          sizes="sm:100vw md:50vw lg:33vw"
+          sizes="sm:100vw md:50vw lg:25vw"
           quality="75"
           class="w-full h-auto object-cover rounded-xl"
           loading="lazy"
         />
-      </div>
+      </a>
     </div>
   </div>
 </template>
