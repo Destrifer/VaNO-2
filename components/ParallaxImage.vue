45f6318ab8789f9eaf142ae85daff46dd89194a4
Destrifer <destrifer@yandex.ru>
2025-06-07 02:12:16 +0300

    fix

--- components/ParallaxImage.vue
+++ components/ParallaxImage.vue
@@ -1,19 +1,24 @@
 <template>
   <div
     ref="tiltRef"
-    class="w-full relative mx-auto my-4 rounded-xl shadow-md border border-gray-200"
+    class="w-full relative mx-auto my-4 rounded-xl shadow-md border border-gray-200 cursor-pointer"
     :class="aspectClass"
     data-tilt
-    data-tilt-max="20"
-    data-tilt-scale="1.05"
+    data-tilt-max="3"
+    data-tilt-scale="1.03"
     data-tilt-speed="500"
-    data-tilt-perspective="1000"
+    data-tilt-perspective="500"
     :style="tiltStyle"
   >
     <!-- Парящий текст (пока без стилей, только положение) -->
     <h2
-      class="absolute left-1/2 bottom-[30%] -translate-x-1/2 text-black text-lg font-bold"
-      style="transform: translateZ(50px)"
+      class="absolute left-1/2 bottom-[10%] -translate-x-1/2 text-3xl font-extrabold text-black text-center pointer-events-none"
+      style="
+        transform: translateZ(50px);
+        -webkit-text-stroke: 1px white;
+        text-shadow: 1px 1px 0 #fff, 2px 2px 0 #bbb,
+          3px 3px 1px rgba(0, 0, 0, 0.2);
+      "
     >
       {{ caption }}
     </h2>
